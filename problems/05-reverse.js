9edabcfcc4a8a51fc4f29bd67c71c6f89fface19
Henry W <henry.woodmansee@gmail.com>
2022-10-11 13:22:13 -0700

    solved 05-reverse.js

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -13,11 +13,31 @@ reverse(""); // ""
 ***********************************************************************/
 
 
-function reverse(str) {
+function reverse(str, reverseString = '') {
+  debugger
+  if (reverseString.length >= str.length) {
+    debugger
+    return reverseString
+    debugger
+  }
+  debugger
+  let lastLetter = str[str.length - 1 - reverseString.length]
+  debugger
 
+  debugger
+  reverseString += lastLetter
+  debugger
+
+  debugger
+  let nextStep = reverse(str, reverseString)
+  debugger
+
+  debugger
+  return nextStep
+  debugger
 }
 
-
+reverse('house')
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
